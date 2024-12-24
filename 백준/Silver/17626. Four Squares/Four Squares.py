c080f0ac748801368b17a3d6f3055a27987ffbfd
강민정 <138704233+MinJeonging@users.noreply.github.com>
2024-12-24 23:00:28 +0900

    [Silver III] Title: Four Squares, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+def checker(n):
+
+    if n == 7:
+
+        return 4
+
+    squares = set([i**2 for i in range(1, 224)])  # 제곱수 집합 생성
+
+    if n in squares:
+
+        return 1
+
+    for square in squares:
+
+        if n - square in squares:
+
+            return 2
+
+    while n % 4 == 0:
+
+        n //= 4
+
+    if (n - 7) % 8 == 0:
+
+        return 4
+
+    return 3
+
+n = int(input())
+
+print(checker(n))
