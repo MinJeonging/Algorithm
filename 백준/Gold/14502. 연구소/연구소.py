958629686ecba6fa4ff2fe363071ec8a98c8a369
강민정 <138704233+MinJeonging@users.noreply.github.com>
2025-01-06 21:25:00 +0900

    [Gold IV] Title: 연구소, Time: 6564 ms, Memory: 36068 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,33 +1,20 @@
-# 14502
+# 14502 연구소
 from itertools import combinations
-import copy
 from collections import deque
 
 # 입력 받기
 N, M = map(int, input().split())
-A = [[0] for _ in range(N)]
+B = [[0] for _ in range(N)]
 
 for i in range(N):
-    A[i] = list(map(int, input().split()))
+    B[i] = list(map(int, input().split()))
 
-# 임의의 3개의 빈 칸 선정
-filled = set()
-maxSafeArea = 0
-for i in range(N):
-    for j in range(M):
-        if A[i][j] == 1 or A[i][j] == 2: # 빈칸이 아닌 경우
-            filled.add(i * M + j)
+cells = [(i, j) for i in range(N) for j in range(M) if B[i][j] == 0]
 
-for combination in combinations(list(range(N * M)), 3):
-    impossible = False
-    B = copy.deepcopy(A) # 모든 조합마다 A deepcopy
-    for space in combination:
-        if space in filled:
-            impossible = True
-            break
-        B[space // M][space % M] = 1
-    if impossible: # space가 하나라도 채워져있는 공간이면 pass
-        continue
+max_safe =0
+for combination in combinations(cells, 3): # 튜플도 조합 가능
+    for row, col in combination: # 벽 세우기
+        B[row][col] = 1
 
     # BFS 수행
 
@@ -55,12 +42,15 @@ for combination in combinations(list(range(N * M)), 3):
                 visit[r + dr[i]][c + dc[i]] = True
 
     # 바이러스가 접근 불가능한 최대 개수 계산
-    safeArea = 0
+    safe = 0
     for i in range(N):
         for j in range(M):
             if visit[i][j] == False and B[i][j] == 0:
-                safeArea += 1
-    if maxSafeArea < safeArea:
-        maxSafeArea = safeArea
+                safe += 1
+    if max_safe < safe:
+        max_safe = safe
 
-print(maxSafeArea)
+    for row, col in combination: # 벽 허물기
+        B[row][col] = 0
+
+print(max_safe)
